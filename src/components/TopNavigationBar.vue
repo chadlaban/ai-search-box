932b0593503768633957ca9429439544c01a36d3
chadlaban <laban.chad@gmail.com>
2023-08-22 15:17:59 +0800

    Added adjustment and on hover state for navigation bar.

--- src/components/TopNavigationBar.vue
+++ src/components/TopNavigationBar.vue
@@ -9,18 +9,18 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item" :class="{ 'active': $route.path === '/featured' }">
+          <li class="nav-item nav-item-hover" :class="{ 'active': $route.path === '/featured' }">
             <router-link class="nav-link" to="/featured">Featured</router-link>
           </li>
           <!-- <li class="nav-item" :class="{ 'active': $route.path === '/my-favorites' }">
             <router-link class="nav-link" to="/my-favorites">My Favorites</router-link>
           </li> -->
-          <li class="nav-item" :class="{ 'active': $route.path === '/community' }">
+          <li class="nav-item nav-item-hover" :class="{ 'active': $route.path === '/community' }">
             <router-link class="nav-link" to="/community">Community</router-link>
           </li>
-          <li class="nav-item" :class="{ 'active': $route.path === '/sign-in' }">
+          <!-- <li class="nav-item" :class="{ 'active': $route.path === '/sign-in' }">
             <router-link class="nav-link" to="/sign-in">Sign In</router-link>
-          </li>
+          </li> -->
           &nbsp;
           &nbsp;
           &nbsp;
@@ -54,24 +54,41 @@ export default {
   .navbar-brand {
     position: relative;
     bottom: 1px;
+    color: #FFFFFF;
     /* left: 24%; */
   }
 
   .navbar-nav {
     max-width: max-content;
     margin: auto;
+
+    .nav-item {
+      border-radius: 25px;
+    }
+
+    > li {
+      margin: 0px 5px 0px 5px;
+    }
   }
 }
 
 .navbar-nav .nav-item.active .nav-link {
   /* background: #D9D9D9; */
   color: #000000 !important;
-  border-radius: 10px;
+  border-radius: 25px;
   font-weight: 500;
+  background-color: #FFFFFF;
 }
 
 .navbar-nav .nav-item .nav-link {
   color: #FFFFFF;
+}
+
+.nav-item-hover:hover {
+  /* Define the hover effect styles here */
+  background-color: #e29f37; /* Example color */
+  color: white; /* Example color */
+  /* Add any other hover styles you want */
 }
 
 .bg-light {
