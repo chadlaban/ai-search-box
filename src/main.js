bafaf7dc7824caaf15f853669e586104a323eaeb
chadlaban <laban.chad@gmail.com>
2023-08-29 14:32:27 +0800

    Updated import bootstrap files.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap";
-import "jquery";
-import "popper.js";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap/dist/js/bootstrap.js";
 import "@fortawesome/fontawesome-free/css/all.css";
 import "@fortawesome/fontawesome-free/js/all.js";
 
